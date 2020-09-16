e914bb9a60985443421529eff9a05217a21b8f4a
megan <msmeganmcmanus@gmail.com>
2020-09-15 18:33:47 -0700

    final project set up

--- joinsEx/queries.sql
+++ joinsEx/queries.sql
@@ -16,8 +16,6 @@ GROUP BY (first_name, last_name
 ) 
 ORDER BY first_name;
 
-
-
 -- Count the number of cars for each owner and display the average price for each of the cars 
 -- as integers. Display the owners first_name, last_name, average price and count of vehicles. The 
 -- first_name should be ordered in descending order. Only display results with more than one vehicle
